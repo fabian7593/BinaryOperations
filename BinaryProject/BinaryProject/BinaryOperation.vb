9af6c8825f0a241c7ec99236566ad6cbb7c32f29
Fabian Rosales <fabian7593@gmail.com>
2017-05-25 23:39:51 -0600

    Frosales, add code of binary project

---
+++ BinaryProject/BinaryProject/BinaryOperation.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Text.RegularExpressions
+Public Class BinaryOperation
+    'Realiza la suma de binarios e imprime en consola
+    Public Sub sum(numOne As String, numTwo As String)
+        'Convierte los dos números a decimales
+        Dim decOne As Integer = Convert.ToInt32(numOne, 2)
+        Dim decTwo As Integer = Convert.ToInt32(numTwo, 2)
+        'suma los números
+        Dim decResult As Integer = decOne + decTwo
+        'Convierte los números a binarios
+        Dim binaryResult As String = Convert.ToString(decResult, 2)
+
+        ' imprime el resultado
+        Console.WriteLine("")
+        Console.WriteLine("*******************************")
+        Console.WriteLine("El resultado de la suma es: {0}", binaryResult)
+        Console.WriteLine("")
+    End Sub
+
+    'Realiza la resta de binarios e imprime en consola
+    Public Sub subtract(numOne As String, numTwo As String)
+        'Convierte los dos números a decimales
+        Dim decOne As Integer = Convert.ToInt32(numOne, 2)
+        Dim decTwo As Integer = Convert.ToInt32(numTwo, 2)
+        'suma los números
+        Dim decResult As Integer = decOne - decTwo
+        'Convierte los números a binarios
+        Dim binaryResult As String = Convert.ToString(decResult, 2)
+
+        ' imprime el resultado
+        Console.WriteLine("")
+        Console.WriteLine("*******************************")
+        Console.WriteLine("El resultado de la resta es: {0}", binaryResult)
+        Console.WriteLine("")
+    End Sub
+
+
+
+
+    'Retorna los dos números binarios para realizar la operación
+    Public Function ReturnBinaryNumsOperation() As String()
+        Dim binaryNumOne As String = ""
+        Dim binaryNumTwo As String = ""
+
+        binaryNumOne = AddBinaryNum("primer")
+
+        'Si el primero número es binario pregunta por el segundo
+        If (binaryNumOne <> "") Then
+            binaryNumTwo = AddBinaryNum("segundo")
+        End If
+
+        'Si el segundo número es binario, realize la acción de resta
+        If (binaryNumTwo <> "") Then
+            'Crea una lista de string para retornar los dos valores
+            Dim listOfBinaryNums(0 To 1) As String
+            listOfBinaryNums(0) = binaryNumOne
+            listOfBinaryNums(1) = binaryNumTwo
+            Return listOfBinaryNums
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    'Function que retorna el numero binario ingresado por el usuario
+    Function AddBinaryNum(secuenseNumber As String) As String
+        Console.WriteLine("")
+        Console.WriteLine("Ingrése {0} número binario", secuenseNumber)
+
+        'Se crea el string de la respuesta del readline
+        Dim binaryNum As String
+        'Lee la tecla tocada por el usuario  y se lo asigna a binaryNumOne
+        binaryNum = Console.ReadLine()
+        'Válida por medio de la expresión regular si es un número binario o no
+        If (Regex.IsMatch(binaryNum, "^[01]+$") = False) Then
+            Console.WriteLine("Lo sentimos, este número no es binario.")
+            'Retorna un valor vacío
+            Return ""
+        Else
+            'Retorna un número binario
+            Return binaryNum
+        End If
+    End Function
+End Class
---
+++ BinaryProject/BinaryProject/MainClass.vb
@@ -0,0 +1,103 @@
+﻿
+Module MainClass
+
+    'Metodo inicial del sistema
+    Sub Main()
+
+        TitleBinaryCalculator()
+        CalculatorOperationCore()
+
+    End Sub
+
+    'El núcleo de las operaciones del sistema
+    Sub CalculatorOperationCore()
+
+        'Realiza la instancia de la clase BinaryOperation que tiene toda la lógica
+        ' de operaciones binarias
+        Dim binaryOperation As BinaryOperation = New BinaryOperation()
+
+        'Variable boolean para mantenernos en el loop
+        Dim stayInWhile As Boolean = True
+
+        'Loop para realizar operaciones o salirse de la aplicación
+        Do While stayInWhile
+            MenuBinaryCalculator()
+
+            'Se crea el string de la respuesta del readline
+            Dim menuAnswer As String
+            'Lee la tecla tocada por el usuario  y se lo asigna a menuAnswer
+            menuAnswer = Console.ReadLine()
+            Console.WriteLine(menuAnswer)
+
+            'Realiza una acción de acuerdo a la opción seleccionada por el usuario
+            Select Case menuAnswer
+                Case "A", "a"
+                    Sum(binaryOperation)
+                Case "B", "b"
+                    Subtract(binaryOperation)
+                Case "C", "c"
+                    Console.Clear()
+                    TitleBinaryCalculator()
+                Case "D", "d"
+                    stayInWhile = False
+                Case Else
+                    Console.WriteLine("")
+                    Console.WriteLine("Porfavor, seleccione una tecla válida")
+                    Console.WriteLine("")
+            End Select
+        Loop
+    End Sub
+
+
+    'Método encargado de sumar los números, se pasa por parametro el operador binario
+    Sub Sum(binaryOperation As BinaryOperation)
+        Console.Clear()
+        Console.WriteLine("")
+        Console.WriteLine("Vamos a sumar")
+        'llama los valores binarios y llena la lista para realizar la acción
+        Dim listOfBinaryNums() As String = binaryOperation.ReturnBinaryNumsOperation()
+
+        'Si tiene valores ejecute la accion
+        If (listOfBinaryNums IsNot Nothing) Then
+            'Llama a la clase binaryOperation y ejecuta el método de suma
+            binaryOperation.sum(listOfBinaryNums(0), listOfBinaryNums(1))
+        End If
+
+    End Sub
+
+    'Método encargado de restar los números, se pasa por parametro el operador binario
+    Sub Subtract(binaryOperation As BinaryOperation)
+        Console.Clear()
+        Console.WriteLine("")
+        Console.WriteLine("Vamos a restar")
+        'llama los valores binarios y llena la lista para realizar la acción
+        Dim listOfBinaryNums() As String = binaryOperation.ReturnBinaryNumsOperation()
+
+        'Si tiene valores ejecute la accion
+        If (listOfBinaryNums IsNot Nothing) Then
+            'Llama a la clase binaryOperation y ejecuta el método de resta
+            binaryOperation.subtract(listOfBinaryNums(0), listOfBinaryNums(1))
+        End If
+    End Sub
+
+
+
+    'Imprime en pantalla el título de la calculadora
+    Sub TitleBinaryCalculator()
+        Console.WriteLine("")
+        Console.WriteLine("***************************************")
+        Console.WriteLine("          Calculadora Binaria          ")
+        Console.WriteLine("***************************************")
+    End Sub
+
+    'Imprime en pantalla el menú de la calculadora
+    Sub MenuBinaryCalculator()
+        Console.WriteLine("")
+        Console.WriteLine("Elija una opción")
+        Console.WriteLine("")
+        Console.WriteLine("1. Presione la tecla 'A' para sumar")
+        Console.WriteLine("2. Presione la tecla 'B' para restar")
+        Console.WriteLine("3. Presione la tecla 'C' para limpiar la consola")
+        Console.WriteLine("4. Presione la tecla 'D' Salir")
+    End Sub
+End Module
